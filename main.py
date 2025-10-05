df030dc66e54d9d59496b5720beb860c2916832e
JemKeHU <staslovel1338@gmail.com>
2025-10-05 19:17:00 +0200

    create basic window

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+import tkinter as tk
+
+def main():
+    main_root = tk.Tk()
+    main_root.title("Test1")
+
+    label1 = tk.Label(main_root, text="Test message!")
+    label1.pack()
+
+    button1 = tk.Button(main_root, text="Stop", width=25, command=main_root.destroy)
+    button1.pack()
+
+    main_root.mainloop()
+
+    return 0
+
+main()
