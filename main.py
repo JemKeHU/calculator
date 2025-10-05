8c184631c15978992328f38bad2f366d0a45494f
JemKeHU <staslovel1338@gmail.com>
2025-10-05 19:30:06 +0200

    add icon for app, resolution

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter as tk
 def main():
     main_root = tk.Tk()
     main_root.title("Test1")
+    CALCULATOR_IMAGE = tk.PhotoImage(file="calculator_photo.png")
+    main_root.iconphoto(True, CALCULATOR_IMAGE)
 
     label1 = tk.Label(main_root, text="Test message!")
     label1.pack()
@@ -10,6 +12,7 @@ def main():
     button1 = tk.Button(main_root, text="Stop", width=25, command=main_root.destroy)
     button1.pack()
 
+    main_root.geometry("240x240")
     main_root.mainloop()
 
     return 0
